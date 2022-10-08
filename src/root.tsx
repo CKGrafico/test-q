f77f69a40471a6f1f2a51e0b0baab5452b9e22ef
Quique Fdez Guerra <quique@ckgrafico.com>
2022-10-08 18:24:55 +0300

    s

---
+++ src/root.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+import { Button } from "@papanasi/qwik";
+
+export default component$(() => {
+  return (
+    <div>
+      <Button>Test</Button>
+    </div>
+  );
+});
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(() => {
+  return {
+    plugins: [qwikVite(), tsconfigPaths()]
+  };
+});
